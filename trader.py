936267c4c87588f2b1f4b3a63466e142125ffde3
thevickypedia <vignesh.sivanandarao@gmail.com>
2023-07-21 23:08:03 -0500

    Get detailed signals

--- trader.py
+++ trader.py
@@ -1,4 +1,6 @@
-import pandas as pd
+import pprint
+
+import pandas
 import webull
 
 wb = webull.paper_webull()
@@ -9,7 +11,7 @@ def get_buy_sell_signals(symbol):
     bars = wb.get_bars(stock=symbol, interval='d', count=100)
 
     # Create a DataFrame from the fetched data
-    stock_data = pd.DataFrame(bars)
+    stock_data = pandas.DataFrame(bars)
 
     # Calculate short-term (e.g., 20-day) and long-term (e.g., 50-day) moving averages
     stock_data['SMA_short'] = stock_data['close'].rolling(window=20).mean()
@@ -25,17 +27,27 @@ def get_buy_sell_signals(symbol):
     buy_signals = stock_data[stock_data['Buy Signal']]
     sell_signals = stock_data[stock_data['Sell Signal']]
 
-    print("Buy Signals:")
-    print(len(buy_signals))
+    print(f"Buy Signals: {len(buy_signals)}")
+    print(f"Sell Signals: {len(sell_signals)}")
 
-    print("Sell Signals:")
-    print(len(sell_signals))
-
+    buy_signals_timestamped = {
+        pandas.Timestamp(timestamp).to_pydatetime(): True
+        for timestamp in buy_signals['Buy Signal'].index.values
+    }
+    sell_signals_timestamped = {
+        pandas.Timestamp(timestamp).to_pydatetime(): False
+        for timestamp in sell_signals['Sell Signal'].index.values
+    }
     if len(sell_signals) > len(buy_signals):
         print('ASSESSMENT: SELL')
     else:
         print('ASSESSMENT: BUY')
+    return {
+        k.strftime("%Y-%m-%d"): v
+        for k, v in dict(sorted({**buy_signals_timestamped, **sell_signals_timestamped}.items())).items()
+    }
 
 
 if __name__ == '__main__':
-    get_buy_sell_signals(symbol="AAPL")
+    result = get_buy_sell_signals(symbol="AAPL")
+    pprint.pprint(result)
