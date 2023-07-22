4e4530cace69b3513ad8afd001d93df4cc21ea3e
thevickypedia <vignesh.sivanandarao@gmail.com>
2023-07-22 00:38:56 -0500

    Add an option for analysis duration Add doc strings and type hints

--- trader.py
+++ trader.py
@@ -7,9 +7,24 @@ import webull
 wb = webull.paper_webull()
 
 
-def get_buy_sell_hold_signals(symbol: str) -> Dict[str, str]:
+def get_signals(symbol: str, bar_count: int = 100) -> Dict[str, str]:
+    """Get buy, sell and hold signals for a particular stock.
+
+    Args:
+        symbol: Stock ticker.
+        bar_count: Number of bars from webull.
+
+    See Also:
+        - A larger `bar_count` gives longer historical data for trend analysis.
+        - A smaller count focuses on recent data for short-term signals.
+        - Experiment and backtest to find the best fit for your approach.
+
+    Returns:
+        Dict[str, str]:
+        A dictionary of each day's buy, sell and hold signals.
+    """
     # Fetch historical stock data using the 'get_bars' method from the 'webull' package
-    bars = wb.get_bars(stock=symbol, interval='d', count=100)
+    bars = wb.get_bars(stock=symbol, interval='d', count=bar_count)
 
     # Create a DataFrame from the fetched data
     stock_data = pandas.DataFrame(bars)
@@ -19,24 +34,14 @@ def get_buy_sell_hold_signals(symbol: str) -> Dict[str, str]:
     stock_data['SMA_long'] = stock_data['close'].rolling(window=50).mean()
 
     # Generate the buy, sell, and hold signals
-    stock_data['Buy Signal'] = stock_data['SMA_short'] > stock_data['SMA_long']
-    stock_data['Sell Signal'] = stock_data['SMA_short'] < stock_data['SMA_long']
-    stock_data['Hold Signal'] = ~(stock_data['Buy Signal'] | stock_data['Sell Signal'])
+    stock_data['buy'] = stock_data['SMA_short'] > stock_data['SMA_long']
+    stock_data['sell'] = stock_data['SMA_short'] < stock_data['SMA_long']
+    stock_data['hold'] = ~(stock_data['buy'] | stock_data['sell'])
 
-    # Filter and display the buy, sell, and hold signals
-    buy_signals = stock_data[stock_data['Buy Signal']]
-    sell_signals = stock_data[stock_data['Sell Signal']]
-    hold_signals = stock_data[stock_data['Hold Signal']]
-
-    assessment = {
-        "Buy": len(buy_signals),
-        "Sell": len(sell_signals),
-        "Hold": len(hold_signals)
-    }
-    for key, value in assessment.items():
-        print(f"{key} Signals:", value)
-
-    print(f"Algorithm's assessment: {max(assessment, key=assessment.get)}")
+    # Filter and display the buy, sell, and holds
+    buy_signals = stock_data[stock_data['buy']]
+    sell_signals = stock_data[stock_data['sell']]
+    hold_signals = stock_data[stock_data['hold']]
 
     buy_signals_timestamped = {
         pandas.Timestamp(timestamp).to_pydatetime(): "Buy"
@@ -56,14 +61,25 @@ def get_buy_sell_hold_signals(symbol: str) -> Dict[str, str]:
         key=lambda x: x[0].timestamp()
     ))
 
-    assert len(all_signals) == len(stock_data), "Not all bars were accounted for stock signals."
+    all_signals_ct = len(all_signals)
+    assert all_signals_ct == len(stock_data), "Not all bars were accounted for stock signals."
+
+    assessment = {
+        "Buy": round(len(buy_signals) / all_signals_ct * 100, 2),
+        "Sell": round(len(sell_signals) / all_signals_ct * 100, 2),
+        "Hold": round(len(hold_signals) / all_signals_ct * 100, 2)
+    }
+    for key, value in assessment.items():
+        print(f"{key} Signals:", f"{value}%")
+
+    print(f"\nAlgorithm's assessment: {max(assessment, key=assessment.get).upper()}\n")
 
     return {
-        k.strftime("%Y-%m-%d"): v
+        k.strftime("%A - %Y-%m-%d"): v
         for k, v in all_signals.items()
     }
 
 
 if __name__ == '__main__':
-    result = get_buy_sell_hold_signals(symbol="EXPE")
-    pprint.pprint(result)
+    result = get_signals(symbol="EXPE", bar_count=1000)
+    pprint.pprint(result, sort_dicts=False)
